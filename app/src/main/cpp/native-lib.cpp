53d551ef036fee1be0c91ecebd7e120bd8c80115
Evgeniy Tabatsky <tabatsky.evgeniy@gene.com>
2024-03-19 17:58:30 +1000

    initial commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,235 @@
+#include <jni.h>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <assert.h>
+#include "sox.h"
+
+void sox_convert(char* inPathCStr, char* outPathCStr);
+void sox_tempo(char* inPathCStr, char* outPathCStr, char* tempoCStr);
+void sox_reverse(char* inPathCStr, char* outPathCStr);
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_jatx_soxtest_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_jatx_soxtest_MainActivity_convertAudioFileJNI(
+        JNIEnv* env,
+        jobject /* this */,
+        jstring inPath,
+        jstring outPath) {
+    char* inPathCStr;
+    char* outPathCStr;
+    inPathCStr = (char*) env->GetStringUTFChars(inPath, NULL);
+    outPathCStr = (char*) env->GetStringUTFChars(outPath, NULL);
+    sox_convert(inPathCStr, outPathCStr);
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_jatx_soxtest_MainActivity_applyTempoJNI(
+        JNIEnv* env,
+        jobject /* this */,
+        jstring inPath,
+        jstring outPath,
+        jstring tempo
+        ) {
+    char* inPathCStr;
+    char* outPathCStr;
+    char* tempoCStr;
+    inPathCStr = (char*) env->GetStringUTFChars(inPath, NULL);
+    outPathCStr = (char*) env->GetStringUTFChars(outPath, NULL);
+    tempoCStr = (char*) env->GetStringUTFChars(tempo, NULL);
+    sox_tempo(inPathCStr, outPathCStr, tempoCStr);
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_jatx_soxtest_MainActivity_applyReverseJNI(
+        JNIEnv* env,
+        jobject /* this */,
+        jstring inPath,
+        jstring outPath
+        ) {
+    char* inPathCStr;
+    char* outPathCStr;
+    inPathCStr = (char*) env->GetStringUTFChars(inPath, NULL);
+    outPathCStr = (char*) env->GetStringUTFChars(outPath, NULL);
+    sox_reverse(inPathCStr, outPathCStr);
+}
+
+void sox_convert(char* inPathCStr, char* outPathCStr) {
+    static sox_format_t * in, * out; /* input and output files */
+    sox_effects_chain_t * chain;
+    sox_effect_t * e;
+    sox_signalinfo_t interm_signal;
+    char * args[10];
+
+    /* All libSoX applications must start by initialising the SoX library    */
+    assert(sox_init() == SOX_SUCCESS);
+
+    /* Open the input file (with default parameters) */
+    assert(in = sox_open_read(inPathCStr, NULL, NULL, NULL));
+
+    interm_signal = in->signal;
+
+    /* Open the output file; we must specify the output signal characteristics.
+    * Since we are using only simple effects, they are the same as the input
+    * file characteristics */
+    assert(out = sox_open_write(outPathCStr, &interm_signal, NULL, NULL, NULL, NULL));
+
+    /* Create an effects chain; some effects need to know about the input
+    * or output file encoding so we provide that information here */
+    chain = sox_create_effects_chain(&in->encoding, &out->encoding);
+
+    /* The first effect in the effect chain must be something that can source
+    * samples; in this case, we use the built-in handler that inputs
+    * data from an audio file */
+    e = sox_create_effect(sox_find_effect("input"));
+    args[0] = (char *)in, assert(sox_effect_options(e, 1, args) == SOX_SUCCESS);
+    /* This becomes the first `effect' in the chain */
+    assert(sox_add_effect(chain, e, &interm_signal, &interm_signal) == SOX_SUCCESS);
+    free(e);
+
+    /* The last effect in the effect chain must be something that only consumes
+    * samples; in this case, we use the built-in handler that outputs
+    * data to an audio file */
+    e = sox_create_effect(sox_find_effect("output"));
+    args[0] = (char *)out, assert(sox_effect_options(e, 1, args) == SOX_SUCCESS);
+    assert(sox_add_effect(chain, e, &interm_signal, &interm_signal) == SOX_SUCCESS);
+    free(e);
+
+    /* Flow samples through the effects processing chain until EOF is reached */
+    sox_flow_effects(chain, NULL, NULL);
+
+    /* All done; tidy up: */
+    sox_delete_effects_chain(chain);
+    sox_close(out);
+    sox_close(in);
+    sox_quit();
+}
+
+void sox_tempo(char* inPathCStr, char* outPathCStr, char* tempoCStr) {
+    static sox_format_t * in, * out; /* input and output files */
+    sox_effects_chain_t * chain;
+    sox_effect_t * e;
+    sox_signalinfo_t interm_signal;
+    char * args[10];
+
+    /* All libSoX applications must start by initialising the SoX library    */
+    assert(sox_init() == SOX_SUCCESS);
+
+    /* Open the input file (with default parameters) */
+    assert(in = sox_open_read(inPathCStr, NULL, NULL, NULL));
+
+    interm_signal = in->signal;
+
+    /* Open the output file; we must specify the output signal characteristics.
+    * Since we are using only simple effects, they are the same as the input
+    * file characteristics */
+    assert(out = sox_open_write(outPathCStr, &interm_signal, NULL, NULL, NULL, NULL));
+
+    /* Create an effects chain; some effects need to know about the input
+    * or output file encoding so we provide that information here */
+    chain = sox_create_effects_chain(&in->encoding, &out->encoding);
+
+    /* The first effect in the effect chain must be something that can source
+    * samples; in this case, we use the built-in handler that inputs
+    * data from an audio file */
+    e = sox_create_effect(sox_find_effect("input"));
+    args[0] = (char *)in, assert(sox_effect_options(e, 1, args) == SOX_SUCCESS);
+    /* This becomes the first `effect' in the chain */
+    assert(sox_add_effect(chain, e, &interm_signal, &interm_signal) == SOX_SUCCESS);
+    free(e);
+
+    /* Create the `tempo' effect, and initialise it with the desired parameters: */
+    e = sox_create_effect(sox_find_effect("tempo"));
+    args[0] = tempoCStr, assert(sox_effect_options(e, 1, args) == SOX_SUCCESS);
+    /* Add the effect to the end of the effects processing chain: */
+    assert(sox_add_effect(chain, e, &interm_signal, &interm_signal) == SOX_SUCCESS);
+    free(e);
+
+    /* The last effect in the effect chain must be something that only consumes
+    * samples; in this case, we use the built-in handler that outputs
+    * data to an audio file */
+    e = sox_create_effect(sox_find_effect("output"));
+    args[0] = (char *)out, assert(sox_effect_options(e, 1, args) == SOX_SUCCESS);
+    assert(sox_add_effect(chain, e, &interm_signal, &interm_signal) == SOX_SUCCESS);
+    free(e);
+
+    /* Flow samples through the effects processing chain until EOF is reached */
+    sox_flow_effects(chain, NULL, NULL);
+
+    /* All done; tidy up: */
+    sox_delete_effects_chain(chain);
+    sox_close(out);
+    sox_close(in);
+    sox_quit();
+}
+
+void sox_reverse(char* inPathCStr, char* outPathCStr) {
+    static sox_format_t * in, * out; /* input and output files */
+    sox_effects_chain_t * chain;
+    sox_effect_t * e;
+    sox_signalinfo_t interm_signal;
+    sox_signalinfo_t interm_signal_2;
+    char * args[10];
+
+    char * tmp_path = "/sdcard/Android/data/jatx.soxtest/files";
+    sox_globals.tmp_path = tmp_path;
+
+    /* All libSoX applications must start by initialising the SoX library    */
+    assert(sox_init() == SOX_SUCCESS);
+
+    /* Open the input file (with default parameters) */
+    assert(in = sox_open_read(inPathCStr, NULL, NULL, NULL));
+
+    interm_signal = in->signal;
+
+    /* Open the output file; we must specify the output signal characteristics.
+    * Since we are using only simple effects, they are the same as the input
+    * file characteristics */
+    assert(out = sox_open_write(outPathCStr, &interm_signal, NULL, NULL, NULL, NULL));
+
+    interm_signal_2 = out->signal;
+
+    /* Create an effects chain; some effects need to know about the input
+    * or output file encoding so we provide that information here */
+    chain = sox_create_effects_chain(&in->encoding, &out->encoding);
+
+    /* The first effect in the effect chain must be something that can source
+    * samples; in this case, we use the built-in handler that inputs
+    * data from an audio file */
+    e = sox_create_effect(sox_find_effect("input"));
+    args[0] = (char *)in, assert(sox_effect_options(e, 1, args) == SOX_SUCCESS);
+    /* This becomes the first `effect' in the chain */
+    assert(sox_add_effect(chain, e, &interm_signal, &interm_signal) == SOX_SUCCESS);
+    free(e);
+
+    /* Create the `reverse' effect, and initialise it with the desired parameters: */
+    e = sox_create_effect(sox_find_effect("reverse"));
+    assert(sox_effect_options(e, 0, args) == SOX_SUCCESS);
+    /* Add the effect to the end of the effects processing chain: */
+    assert(sox_add_effect(chain, e, &interm_signal, &interm_signal) == SOX_SUCCESS);
+    free(e);
+
+    /* The last effect in the effect chain must be something that only consumes
+    * samples; in this case, we use the built-in handler that outputs
+    * data to an audio file */
+    e = sox_create_effect(sox_find_effect("output"));
+    args[0] = (char *)out, assert(sox_effect_options(e, 1, args) == SOX_SUCCESS);
+    assert(sox_add_effect(chain, e, &interm_signal, &interm_signal) == SOX_SUCCESS);
+    free(e);
+
+    /* Flow samples through the effects processing chain until EOF is reached */
+    sox_flow_effects(chain, NULL, NULL);
+
+    /* All done; tidy up: */
+    sox_delete_effects_chain(chain);
+    sox_close(out);
+    sox_close(in);
+    sox_quit();
+}
---
+++ app/src/main/java/jatx/soxtest/MainActivity.kt
@@ -0,0 +1,261 @@
+package jatx.soxtest
+
+import android.Manifest
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
+import jatx.soxtest.databinding.ActivityMainBinding
+import org.apache.commons.io.FileUtils
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val openAudioFileLauncher = registerForActivityResult(
+        ActivityResultContracts.OpenDocument()
+    ) { uri ->
+        uri?.let { theUri ->
+           tryLoadAudioFileFromUri(theUri)
+        }
+    }
+
+    private val saveAudioFileLauncher = registerForActivityResult(
+        ActivityResultContracts.CreateDocument("*/*")
+    ) { uri ->
+        uri?.let { theUri ->
+            trySaveAudioFileToUri(theUri)
+        }
+    }
+
+    private val tmpFiles = arrayListOf<File>()
+
+    private var currentProjectFile: File? = null
+    private var outFile: File? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        cleanProject()
+
+        // Example of a call to a native method
+        binding.btnLoadFile.setOnClickListener {
+            tryOpenAudioFile()
+        }
+
+        binding.btnSaveMp3.setOnClickListener {
+            trySaveAudioFile("mp3")
+        }
+
+        binding.btnApplyTempo.setOnClickListener {
+            val tempo = binding.etTempo.text.toString()
+                .takeIf { it.isNotEmpty() }
+                ?.toFloat()
+                ?.takeIf { it > 0 } ?: 1.0f
+            applyTempo(tempo)
+        }
+
+        binding.btnApplyReverse.setOnClickListener {
+            applyReverse()
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+    }
+
+    private fun cleanProject() {
+        tmpFiles.clear()
+        currentProjectFile = null
+        outFile = null
+        FileUtils.cleanDirectory(getProjectDir())
+    }
+
+    private fun getProjectDir() = getExternalFilesDir(null)
+
+    private fun setButtonsEnables(enabled: Boolean) {
+        binding.btnLoadFile.isEnabled = enabled
+        binding.btnSaveMp3.isEnabled = enabled
+        binding.btnApplyTempo.isEnabled = enabled
+        binding.btnApplyReverse.isEnabled = enabled
+    }
+
+    private fun tryOpenAudioFile() {
+        openAudioFileLauncher.launch(arrayOf("*/*"))
+    }
+
+    private fun trySaveAudioFile(extension: String) {
+        tmpFiles.lastOrNull()?.let { lastFile ->
+            outFile = generateTmpFileFromCurrentDate(extension)
+            convertAudioFileJNI(lastFile.absolutePath, outFile!!.absolutePath)
+        }
+        currentProjectFile?.let {
+            val fileName = "${it.nameWithoutExtension}.${extension}"
+            saveAudioFileLauncher.launch(fileName)
+        }
+    }
+
+    private fun generateTmpFileFromCurrentDate(extension: String): File {
+        val sdf = SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault())
+        val dateStr = sdf.format(Date())
+        val fileName = "${dateStr}.${extension}"
+        return File(getProjectDir(), fileName)
+    }
+
+    private fun copyFileAndGetPath(uri: Uri): String? {
+        val cursor = contentResolver.query(uri, null, null, null, null) ?: return null
+        val columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME)
+        cursor.moveToFirst()
+        val displayName = cursor.getString(columnIndex)
+        cursor.close()
+
+        val inputStream = contentResolver.openInputStream(uri)
+        val newFile = File(getProjectDir(), displayName)
+        val fileOutputStream = FileOutputStream(newFile)
+
+        inputStream?.copyTo(fileOutputStream)
+
+        fileOutputStream.flush()
+        fileOutputStream.close()
+        inputStream?.close()
+
+        tmpFiles.add(newFile)
+        currentProjectFile = newFile
+
+        return newFile.absolutePath
+    }
+
+    private fun copyOutFileToUri(uri: Uri) {
+        val outputStream = contentResolver.openOutputStream(uri)
+        outFile?.let { theOutFile ->
+            val fileInputStream = FileInputStream(theOutFile)
+
+            outputStream?.let {
+                fileInputStream.copyTo(it)
+                it.flush()
+                it.close()
+            }
+
+            fileInputStream.close()
+        }
+    }
+
+    private fun tryLoadAudioFileFromUri(uri: Uri) {
+        val permissionListener = object: PermissionListener {
+            override fun onPermissionGranted() {
+                setButtonsEnables(false)
+                cleanProject()
+                copyFileAndGetPath(uri)?.let { origPath ->
+                    val tmpFile = generateTmpFileFromCurrentDate("wav")
+                    convertAudioFileJNI(origPath, tmpFile.absolutePath)
+                    tmpFiles.add(tmpFile)
+                }
+                setButtonsEnables(true)
+            }
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                Log.e("access", "no sdcard access")
+            }
+        }
+
+        checkMediaPermissions(permissionListener)
+    }
+
+    private fun trySaveAudioFileToUri(uri: Uri) {
+        val permissionListener = object: PermissionListener {
+            override fun onPermissionGranted() {
+                setButtonsEnables(false)
+                copyOutFileToUri(uri)
+                setButtonsEnables(true)
+            }
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                Log.e("access", "no sdcard access")
+            }
+        }
+
+        checkMediaPermissions(permissionListener)
+    }
+
+    private fun checkMediaPermissions(permissionListener: PermissionListener) {
+        if (Build.VERSION.SDK_INT >= 33) {
+            TedPermission.create()
+                .setPermissionListener(permissionListener)
+                .setPermissions(
+                    Manifest.permission.READ_MEDIA_AUDIO
+                )
+                .check()
+        } else if (Build.VERSION.SDK_INT >= 30) {
+            TedPermission.create()
+                .setPermissionListener(permissionListener)
+                .setPermissions(
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                .check()
+        } else {
+            TedPermission.create()
+                .setPermissionListener(permissionListener)
+                .setPermissions(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+                .check()
+        }
+    }
+
+    private fun applyTempo(tempo: Float) {
+        setButtonsEnables(false)
+        tmpFiles.lastOrNull()?.let { inFile ->
+            val newFile = generateTmpFileFromCurrentDate("wav")
+            applyTempoJNI(inFile.absolutePath, newFile.absolutePath, tempo.toString())
+            tmpFiles.add(newFile)
+        }
+        setButtonsEnables(true)
+    }
+
+    private fun applyReverse() {
+        setButtonsEnables(false)
+        tmpFiles.lastOrNull()?.let { inFile ->
+            val newFile = generateTmpFileFromCurrentDate("wav")
+            applyReverseJNI(inFile.absolutePath, newFile.absolutePath)
+            tmpFiles.add(newFile)
+        }
+        setButtonsEnables(true)
+    }
+
+    /**
+     * A native method that is implemented by the 'soxtest' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    external fun convertAudioFileJNI(inPath: String, outPath: String)
+
+    external fun applyTempoJNI(inPath: String, outPath: String, tempo: String)
+
+    external fun applyReverseJNI(inPath: String, outPath: String)
+
+    companion object {
+        // Used to load the 'soxtest' library on application startup.
+        init {
+            System.loadLibrary("sox")
+            System.loadLibrary("soxtest")
+        }
+    }
+}
